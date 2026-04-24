c35181dc72c8ec307993428694f0c48e3d748c54
Eric Brousseau <emb417@gmail.com>
2026-04-24 16:19:27 -0700

    minor: updated layout metadata and added robots.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 export const metadata = {
   title: "mist + lore — Aromatherapy Essentials",
   description:
-    "An heirloom approach to elevated aromatherapy. Whether you're looking for a sense of calm, uplifting engergy, or anything in-between, mist + lore is a place for you to create the perfect atmosphere. Sprays, salts and scrubs, hot and cold therapy, and incense. All items are handcrafted in small batches, or made to order in Portland, Oregon.",
+    "Sprays, salts and scrubs, hot and cold therapy, and incense. All items are handcrafted in small batches, or made to order in Portland, Oregon.",
   metadataBase: new URL("https://www.mistandlore.com"),
   openGraph: {
     title: "mist + lore",
